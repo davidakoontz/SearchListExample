4890e7f3d59b570074aeca369e1f3419a7d55a06
davidakoontz <david@koontz.name>
2022-09-13 10:05:54 -0500

    Initial Commit

---
+++ SearchListExample/SearchListExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchListExampleApp.swift
+//  SearchListExample
+//
+//  Created by David on 9/13/22.
+//
+
+import SwiftUI
+
+@main
+struct SearchListExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
